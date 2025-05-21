ea9a09820af29f750c17a9fd478b3ec52a62e77c
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2025-05-21 21:45:55 +1000

    Add project schema definition for Sanity

---
+++ app/sanity/schemas/schema.ts
@@ -0,0 +1,34 @@
+import { defineField } from "sanity";
+
+const project = {
+  name: "project",
+  title: "projects",
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "name" },
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "image",
+      fields: [
+        defineField({
+          name: "alt",
+          type: "string",
+          title: "Alternative text",
+        }),
+      ],
+    }),
+  ],
+};
+
+export default project;
