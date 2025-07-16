f0db795aa76345ffe196263fb99d2a00535f1985
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2025-07-16 10:57:20 +1000

    feat: update project parameter handling to use Promise for improved async processing

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -3,13 +3,11 @@ import { getProject } from "@/sanity/sanity-utils";
 import { notFound } from "next/navigation";
 
 type Props = {
-  params: {
-    project: string;
-  };
+  params: Promise<{ project: string }>;
 };
 
 export default async function Project({ params }: Props) {
-  const slug = params.project;
+  const { project: slug } = await params;
   const project = await getProject(slug);
 
   if (!project) {
