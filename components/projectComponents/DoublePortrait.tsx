37476b37db897bc3f4523b375608482cc7da874f
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2025-07-23 14:05:10 +1000

    feat: implement lightbox component for enhanced image viewing experience and update image styles for consistency

--- components/projectComponents/DoublePortrait.tsx
+++ components/projectComponents/DoublePortrait.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import { urlFor } from "@/sanity/lib/image";
 import { SINGLE_PROJECT_QUERYResult } from "@/sanity/types";
 import Image from "next/image";
 
 import "./DoublePortrait.css";
 import "@/app/grid.css";
+import { useState } from "react";
+import { Lightbox } from "../Lightbox";
 
 type doublePortraitProps = Extract<
   NonNullable<NonNullable<SINGLE_PROJECT_QUERYResult>["content"]>[number],
@@ -11,12 +15,23 @@ type doublePortraitProps = Extract<
 >;
 
 export function DoublePortrait({ leftImage, rightImage }: doublePortraitProps) {
+  const [activeImage, setActiveImage] = useState<{
+    src: string;
+    alt?: string;
+  } | null>(null);
+
   return (
     <section className="double-portrait">
       <div className="images grid">
         <div className="left-portrait-image">
           {leftImage ? (
             <Image
+              onClick={() =>
+                setActiveImage({
+                  src: urlFor(leftImage).url(),
+                  alt: leftImage.alt || "",
+                })
+              }
               src={urlFor(leftImage).auto("format").quality(90).url()}
               alt={leftImage?.alt ?? ""}
               width={2160}
@@ -28,6 +43,12 @@ export function DoublePortrait({ leftImage, rightImage }: doublePortraitProps) {
         <div className="right-portrait-image">
           {rightImage ? (
             <Image
+              onClick={() =>
+                setActiveImage({
+                  src: urlFor(rightImage).url(),
+                  alt: rightImage.alt || "",
+                })
+              }
               src={urlFor(rightImage).auto("format").quality(90).url()}
               alt={rightImage?.alt ?? ""}
               width={2160}
@@ -37,6 +58,13 @@ export function DoublePortrait({ leftImage, rightImage }: doublePortraitProps) {
           ) : null}
         </div>
       </div>
+      {activeImage && (
+        <Lightbox
+          src={activeImage.src}
+          alt={activeImage.alt || ""}
+          onClose={() => setActiveImage(null)}
+        />
+      )}
     </section>
   );
 }
